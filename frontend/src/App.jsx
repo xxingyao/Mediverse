7636fe049f4699366017ddf5d723a971dd2d4031
Lau Xing Yao <lauxingyao4188@gmail.com>
2025-08-06 18:28:04 +0800

    test tailwind works

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
-export const App = () => {
+const App = () => {
   return (
-    <div>App</div>
+    <div className='text-green-600'> Testing tailwind</div>
   )
 }
+
+
+export default App
