587492c53427a6769f83908bf3dc87ce607024ee
Lau Xing Yao <lauxingyao4188@gmail.com>
2025-08-08 17:31:05 +0800

    add appointment page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Doctors from './pages/Doctors'
+import Login from './pages/Login'
 
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
       <Routes>
         <Route path='/' element={<Home />}/>  
+        <Route path='/doctors' element={<Doctors />}/>
+        <Route path='/doctors/:speciality' element={<Doctors />} /> 
+        <Route path='/login' element={<Login />}/>
       </Routes>   
     </div>
   )
